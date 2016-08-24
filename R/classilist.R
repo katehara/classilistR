92c1f8d0fa55097d2948a8dd481d30a413915172
katehara <medhakat95@gmail.com>
2016-08-24 10:19:37 +0530

    minor fixes

--- R/classilist.R
+++ R/classilist.R
@@ -21,43 +21,41 @@ classilist <- function(dataSet , location , features  , predicted , actual , pro
     names(dataSet) <- gsub(x = colN, pattern = "\\.", replacement = " ")
     num = 1
   }
-  
+
   if(validateColumns(colN , features  , predicted , actual , probs) & validateFile(location) & num == 1){
-    
+
     for(i in 1:length(colN)){
       nm <- colN[i]
       if(is.element(nm , features)){
         rep <- paste('F-',nm ,sep='')
         colN[i] <- rep
       }
-      
+
       else if(is.element(nm , probs)){
         rep <- paste('P-',nm ,sep='')
         colN[i] <- rep
       }
-      
+
       else if(nm == actual){
         rep <- paste('A-',nm ,sep='')
         colN[i] <- rep
       }
-      
+
       else if(nm == predicted){
         rep <- "Predicted"
         colN[i] <- rep
       }
-      
+
       else {
         print(paste("Warning : Column", colN[i], "is not specified in any of the categories", sep=" "))
       }
     }
-    write.csv(dataSet , file = paste(location , "/data/out.csv" , sep = "") , row.names = FALSE) 
+    names(dataSet)<- colN
+    write.csv(dataSet , file = paste(location , "/data/out.csv" , sep = "") , row.names = FALSE)
     browseURL(url = paste("file://" , location , "/index.html" , sep = ""), browser = getOption("browser"),encodeIfNeeded = FALSE)
-  } 
-  
+  }
+
   else {
     print("Error : Process Terminated Unexpetedly due to one or more Reasons.")
   }
 }
-
-
-
